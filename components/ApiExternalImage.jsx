dc8bb1a405ee80f9b35a012ab7847d38ffd2f4cb
orion14th <orion14th@gmail.com>
2024-01-13 16:11:25 -0700

    eleventh

--- components/ApiExternalImage.jsx
+++ components/ApiExternalImage.jsx
@@ -12,10 +12,20 @@ import React, { useState, useEffect } from 'react';
     } from "@/components/ui/carousel"
 
     import Autoplay from "embla-carousel-autoplay"
-
+    import useEmblaCarousel from 'embla-carousel-react'
+ 
     import LoadingComponent from '@/components/LoadingComponent';
+
+
+ 
  
     const ImageSlider = () => {
+
+
+ 
+      const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+
 
 
       const [images, setImages] = useState([]);
@@ -23,20 +33,47 @@ import React, { useState, useEffect } from 'react';
 
       const [isLoading, setIsLoading] = useState(0);
 
+      const [isImagesLoading, setImageLoading] = useState(0);
+
+
       // Fetch images using Next.js's fetch with revalidation
     
       const unsplashAccessKey = 'W4oBf7UVziIqyfjv9Qm6cLJH9Trzbec20B1DXUdfIDE'; // Replace with your Unsplash access key
       const numberOfImages = 5;
-     
+      const numbersToPreload = 3;
+
+ 
        
+ 
+
+function handleLoadedImages(){
+
+  setTimeout(  ()=>{setIsLoading(1);}, 2000) ;  
+  setTimeout(  ()=>{   setIsLoading(2);}, 3000) ; 
+ 
+}
+
+      const handleImageLoad=()=>{
+
+        setImageLoading(isImagesLoading+1);
+
+console.log(`loading number ${isImagesLoading}`);
+
+        if(isImagesLoading==numbersToPreload){
+   
+          handleLoadedImages();
+
+        }
+
+      }
     
     useEffect(() => {
     
      
       const lastFetchTime = localStorage.getItem('lastFetchTime');
-      const shouldFetch = lastFetchTime === null || Date.now() - Number(lastFetchTime) >= 60 * 1000;
+     // const shouldFetch = lastFetchTime === null || Date.now() - Number(lastFetchTime) >= 60 * 1000;
     
-     
+     const shouldFetch=true;
     
       // Fetch images from API if not already in local storage
       if (!localStorage.getItem('images') || shouldFetch) {
@@ -59,8 +96,7 @@ import React, { useState, useEffect } from 'react';
 
 // Set Loading to 1 to fade after 1 second, and 2 to hide after 2 seconds
 
-            setTimeout(  ()=>{setIsLoading(1);}, 2000) ;  
-            setTimeout(  ()=>{setIsLoading(2);}, 3000) ; 
+ 
 
 
           })
@@ -70,9 +106,9 @@ import React, { useState, useEffect } from 'react';
         setImages(storedImages.map((image) => image.urls.regular));
         
 // Set Loading to 1 to fade after 1 second, and 2 to hide after 2 seconds
-
-        setTimeout(  ()=>{setIsLoading(1);}, 1000) ;  
-        setTimeout(  ()=>{setIsLoading(2);}, 2000) ; 
+setTimeout(  ()=>{setIsLoading(1);}, 2000) ;  
+setTimeout(  ()=>{setIsLoading(2);}, 3000) ; 
+  
       }
     }, []);
     
@@ -97,11 +133,7 @@ import React, { useState, useEffect } from 'react';
 
 
         {images.length > 0 && (
-        <Carousel className="CarouseComponent   animate__animated animate__fadeIn "  plugins={[
-        Autoplay({
-          delay:5000,
-        }),
-      ]}>
+        <Carousel  ref={emblaRef} className="CarouseComponent   animate__animated animate__fadeIn " >
           <CarouselContent className="CarouselContentComponent -ml-1">
 
 
@@ -111,7 +143,7 @@ import React, { useState, useEffect } from 'react';
                 <div className="p-1">
                   <Card>
                     <CardContent className="CardContentComponent flex aspect-square items-center justify-center p-6">
-                      <span className="text-2xl font-semibold">   <Image className="CarouselItemImg" src={regularImageUrl} alt={`Image ${index + 1}`} width={400} height={300} />    </span>
+                      <span className="text-2xl font-semibold">   <Image onLoad={handleImageLoad} className="CarouselItemImg" src={regularImageUrl} alt={`Image ${index + 1}`} width={400} height={300} />    </span>
                     </CardContent>
                   </Card>
                 </div>
